c27ba4d0061e01573dc9383e3d2c45a539035499
anegussie <aalem_99@yahoo.com>
2022-09-23 13:05:59 -0400

    Fixed type issues

--- apps/angular-anim/src/app/account-details/account-details.component.ts
+++ apps/angular-anim/src/app/account-details/account-details.component.ts
@@ -1,10 +1,10 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { Observable, of } from 'rxjs';
 import { Router, NavigationEnd  } from '@angular/router';
 // eslint-disable-next-line
 import { AccountService } from 'libs/shared/services/src/lib/account.service';
+// eslint-disable-next-line
 import { Account } from 'libs/shared/services/src/lib/account';
 
 
@@ -19,49 +19,35 @@ import { Account } from 'libs/shared/services/src/lib/account';
 
 export class AccountDetailsComponent implements OnInit {
 
-
-
   accounts: Account[]= <Account[]> {};
   currentRoute = '';
   accountDetail: Account = <Account> {};
   idIndex = 0xFFFFFF;
 
 
-
-
   constructor(private accountService: AccountService, private activatedRoute : ActivatedRoute, private router: Router){}
 
   ngOnInit(): void {
-    this.idIndex = Number(this.router.url.split('/').slice(-1).join(''));
 
-
-     console.log(this.router.url.split('/').slice(-1).join(''));
+    this.idIndex = parseInt(this.router.url.split('/').slice(-1).join(''));
 
      this.accountService.getAccounts().subscribe((accounts) => {
       this.accounts = accounts;
-    });
+     });
 
     this.accountDetail = (this.idIndex>0 && (this.idIndex <= this.accounts.length))? this.accounts[this.idIndex -1]:<Account>{};
-
-
-      this.accountService.accountdetails.subscribe((data) => {
-      this.accounts=<Account[]> data;
-  })
+    this.accountService.accountdetails.subscribe((data) => {
+    this.accounts=<Account[]> data;
+    })
 
   }
 
   isValidInput(): boolean {
-
     const val = parseInt(this.router.url.split('/').slice(-1).join(''));
-
-    console.log(val);
 
     if ((val >0) && (val <= this.accounts.length )) return true;
     return false;
   }
-
-
-
 
 
 }
--- apps/angular-anim/src/app/app-routing.module.ts
+++ apps/angular-anim/src/app/app-routing.module.ts
@@ -4,13 +4,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { AccountDetailsComponent } from './account-details/account-details.component';
-import { TransfersComponent } from './transfers/transfers.component';
+
 
 // TODO: 2. We've setup these routes and have them on the page but they aren't working
 const routes: Routes = [
   { path: '', component: AccountSummaryComponent },
   { path: 'account', component: AccountDetailsComponent, children: [{path: ':id', component: AccountDetailsComponent}] },
- // { path: 'transfers', component: TransfersComponent },
   { path: 'about', component: AboutComponent },
 ];
 
